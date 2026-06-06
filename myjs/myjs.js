d56bc15e532b8f30da7c6b5414b0ea011459fbf9
Mariami <mariam.tch1996@gmail.com>
2026-06-06 19:17:37 +0400

    feat/ added new folder to exercise Javascript topics

---
+++ myjs/myjs.js
@@ -0,0 +1,17 @@
+console.log("This is the first line\nAnd this is the second");
+console.log('A newline character is written like \n"\\n".');
+console.log("con" + "cat" + "e" + "nate");
+console.log(`half of 10 is ${10 / 2}`); // Backtick-quoted strings are usually called: template literals
+
+// Typeof operator
+console.log(typeof 5);
+console.log(typeof "Marimai");
+console.log(typeof [1, 2, 3, 4, 5]);
+console.log(
+  typeof function () {
+    1 + 2;
+  },
+);
+console.log(-(10 - 2));
+
+// Comparisons:
