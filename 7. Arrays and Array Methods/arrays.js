8c585d2c7b500e527bd8fd50756bbdd85c6eb112
Mariami <mariam.tch1996@gmail.com>
2026-06-10 23:43:44 +0400

    strarted  arrays lesson

---
+++ 7. Arrays and Array Methods/arrays.js
@@ -0,0 +1,15 @@
+// Task 1:
+const favFood = ["Khinkali", "Mtsvadi", "Burger", "Fried Chicken", "Sushi"];
+
+// Task 2:
+const colors = ["Red", "Blue", "Green", "Black", "Orange"];
+console.log(colors[0]);
+console.log(colors[4]);
+console.log(colors[2]);
+
+// Task 3:
+const fruitsArr = ["Strawberry", "Apple"];
+fruitsArr.push("Peach"); // add last item
+console.log(fruitsArr);
+fruitsArr.unshift("Orange"); //add first item
+console.log(fruitsArr);
