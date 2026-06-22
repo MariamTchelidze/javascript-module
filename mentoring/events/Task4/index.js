b5dd8f34d884fb77a865f36f0b5d055c4bb7cd6c
Mariami <mariam.tch1996@gmail.com>
2026-06-22 22:56:51 +0400

    started  task 4

---
+++ mentoring/events/Task4/index.js
@@ -0,0 +1,5 @@
+const name = document.getElementById("nameInput");
+const email = document.getElementById("emailInput");
+const password = document.getElementById("passwordInput");
+const submitBtn = document.querySelector("button");
+const paragraph = document.getElementById("formStatus");
