58b6f71324bb389fb759772e89a6777a6ec363d2
Mariami <mariam.tch1996@gmail.com>
2026-06-23 23:01:54 +0400

    added qorking enviroment

---
+++ mentoring/events/Task7/index.js
@@ -0,0 +1,4 @@
+const form = document.querySelector("#todoForm");
+const input = document.querySelector("#todoInput");
+const error = document.querySelector("#todoError");
+const todoList = document.querySelector("#todoList");
